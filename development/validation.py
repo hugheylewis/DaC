248985f461e77095348c3e76d260128316012bc9
hugheylewis <103709866+hugheylewis@users.noreply.github.com>
2024-11-14 18:58:53 -0500

    Update validation.py
    Modify 'check_fields' function

--- development/validation.py
+++ development/validation.py
@@ -5,6 +5,7 @@ import sys
 import os
 import json
 import time
+import github
 
 def colorize(text, color_code, bold=False):
     bold_code = "\033[1m" if bold else ""
@@ -16,21 +17,32 @@ def check_fields(data):
     time.sleep(0.25)
     for field in required_fields:
         if 'module' in data:
-            for module in data['module']:  # Assuming there could be multiple modules in the list, check each one
-                if 'user_added_to_admin_group' in module:
-                    user_group = module['user_added_to_admin_group']
-                    if field not in user_group:
-                        print(colorize("ERROR!", "\033[31m", True) + f" Missing field: '{field}'")
-                        issues.append(f"'{field}' is missing from your {file.name} file")
+            modules = data['module']
+            
+            if isinstance(modules, list): # If 'module' is a list
+                for module_data in modules:
+                    for k, v in module_data.items():
+                        if field not in k:
+                            print(colorize("ERROR!", "\033[31m", True) + f" Missing field: '{field}'")
+                            try:
+                                issues.append(f"'{field}' is missing from your {file.name} file in a module")
+                            except AttributeError as e:
+                                issues.append(f"'{field}' is missing from one of your files")
+                            return False
+
+            elif isinstance(modules, dict): # If 'module' is a dictionary
+                for module_name, module_data in modules.items():
+                  if field not in module_data:
+                        print(colorize("ERROR!", "\033[31m", True) + f" Missing field: '{field}' in module '{module_name}'")
+                        issues.append(f"'{field}' is missing from your {file.name} file in module '{module_name}'")
                         return False
     print(colorize("Success!", "\033[32m", True) + " All required fields are present.")
     return True
 
 if __name__ == "__main__":
     issues = []
-
     required_fields = ['name', 'search', 'cron_schedule', 'is_scheduled', 'alert']
-    terraform = Terraform(working_dir='/home/user/detection-engineering/')
+    terraform = Terraform(working_dir='/home/cam/detection-engineering/')
     validate_return_code, validate_stdout, validate_stderr = terraform.validate()
     fmt_return_code, fmt_stdout, fmt_stderr = terraform.fmt(diff=True)
 
@@ -48,7 +60,7 @@ if __name__ == "__main__":
         issues.append(f"'terraform fmt' returned code {fmt_return_code}")
     time.sleep(0.25)
 
-    for root, dirs, files in os.walk('/home/user/detection-engineering/detections'):
+    for root, dirs, files in os.walk('/home/cam/detection-engineering/detections'):
         for file in files:
             if file.endswith(".tf"):
                 full_path = os.path.join(root, file)
@@ -60,9 +72,16 @@ if __name__ == "__main__":
 
     if len(issues) == 0 and check_fields(json_data):
         push_to_github = input("\n\nAll tests pass! Do you want to push these detections to GitHub? [Y/n] ")
+        while push_to_github not in ['Y', 'n']:
+            print(f"{push_to_github}: invalid answer. Please enter 'Y' or 'n'")
+            push_to_github = input("Do you want to push these detections to GitHub? [Y/n] ")
         if push_to_github == 'Y':
             print("Pushing to remote repository...")
             time.sleep(0.25)
+            github.stage_commit_push()
+        else:
+            print("Detections will not be pushed to GitHub. Exiting...")
+            sys.exit(0)
     else:
         print(f"\n\n{len(issues)} issues were found in your Terraform files:")
         for i in issues:
